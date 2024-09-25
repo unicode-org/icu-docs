c20beb883107d2e5baf07c19977964b42f144689
Craig <ccornelius@google.com>
2024-09-25 08:39:18 -0700

    ICU-22723 Publish 76RC J/C API Docs

---
+++ apidoc/dev/icu4c/search/typedefs_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['olduchar_0',['OldUChar',['../umachine_8h.html#a85b92f7aa77a10e854b4cbff55a7a88c',1,'umachine.h']]]
+];
--- apidoc/dev/icu4c/search/variables_11.js
+++ apidoc/dev/icu4c/search/variables_11.js
@@ -1,15 +1,17 @@
 var searchData=
 [
   ['r_0',['r',['../structUText.html#a88cccfe6b055df7e19f210396fec085a',1,'UText']]],
-  ['replace_1',['replace',['../structUReplaceableCallbacks.html#a6fe116cc3825cc6c749a95f1bd0d2315',1,'UReplaceableCallbacks::replace'],['../structUTextFuncs.html#a8ba9ac339f0e901d87e4568b174943f9',1,'UTextFuncs::replace']]],
-  ['reserved1_2',['reserved1',['../structUTextFuncs.html#a07aae199d0c0c50c843900aac4ab75c6',1,'UTextFuncs']]],
-  ['reserved2_3',['reserved2',['../structUTextFuncs.html#a0a34a019ecb5b8f6c53b640b5b259e94',1,'UTextFuncs']]],
-  ['reserved3_4',['reserved3',['../structUTextFuncs.html#a788850d9f82cf783bbdd731ac5407c2e',1,'UTextFuncs']]],
-  ['reservedb3_5',['reservedB3',['../structUIDNAInfo.html#a9358cdb55cc4055bfe2614482bcbd3df',1,'UIDNAInfo']]],
-  ['reservedbyte_6',['reservedByte',['../structUDataInfo.html#ae2bfb79e1720a9190b9efe1bb446d897',1,'UDataInfo']]],
-  ['reservedfield_7',['reservedField',['../structUCharIterator.html#a5d28abc1c284a02d887651c0594d2181',1,'UCharIterator']]],
-  ['reservedfn_8',['reservedFn',['../structUCharIterator.html#a94b7ab71aadd2cbafa7394c1102cdf2f',1,'UCharIterator']]],
-  ['reservedi2_9',['reservedI2',['../structUIDNAInfo.html#ab5f0eec8c4352d5d64253adeca701c9b',1,'UIDNAInfo']]],
-  ['reservedi3_10',['reservedI3',['../structUIDNAInfo.html#a0831b4791a3627991cf0131edc13acad',1,'UIDNAInfo']]],
-  ['reservedword_11',['reservedWord',['../structUDataInfo.html#af4a5b961c10058651aacb7a2ae45a199',1,'UDataInfo']]]
+  ['rangeend_1',['rangeEnd',['../structU__HEADER__ONLY__NAMESPACE_1_1CodePointRange.html#aac1e1bad2d9facd73a76db75b0a48e4e',1,'U_HEADER_ONLY_NAMESPACE::CodePointRange']]],
+  ['rangestart_2',['rangeStart',['../structU__HEADER__ONLY__NAMESPACE_1_1CodePointRange.html#af7289370c39993b7edf5d1266f8e7bbf',1,'U_HEADER_ONLY_NAMESPACE::CodePointRange']]],
+  ['replace_3',['replace',['../structUReplaceableCallbacks.html#a6fe116cc3825cc6c749a95f1bd0d2315',1,'UReplaceableCallbacks::replace'],['../structUTextFuncs.html#a8ba9ac339f0e901d87e4568b174943f9',1,'UTextFuncs::replace']]],
+  ['reserved1_4',['reserved1',['../structUTextFuncs.html#a07aae199d0c0c50c843900aac4ab75c6',1,'UTextFuncs']]],
+  ['reserved2_5',['reserved2',['../structUTextFuncs.html#a0a34a019ecb5b8f6c53b640b5b259e94',1,'UTextFuncs']]],
+  ['reserved3_6',['reserved3',['../structUTextFuncs.html#a788850d9f82cf783bbdd731ac5407c2e',1,'UTextFuncs']]],
+  ['reservedb3_7',['reservedB3',['../structUIDNAInfo.html#a9358cdb55cc4055bfe2614482bcbd3df',1,'UIDNAInfo']]],
+  ['reservedbyte_8',['reservedByte',['../structUDataInfo.html#ae2bfb79e1720a9190b9efe1bb446d897',1,'UDataInfo']]],
+  ['reservedfield_9',['reservedField',['../structUCharIterator.html#a5d28abc1c284a02d887651c0594d2181',1,'UCharIterator']]],
+  ['reservedfn_10',['reservedFn',['../structUCharIterator.html#a94b7ab71aadd2cbafa7394c1102cdf2f',1,'UCharIterator']]],
+  ['reservedi2_11',['reservedI2',['../structUIDNAInfo.html#ab5f0eec8c4352d5d64253adeca701c9b',1,'UIDNAInfo']]],
+  ['reservedi3_12',['reservedI3',['../structUIDNAInfo.html#a0831b4791a3627991cf0131edc13acad',1,'UIDNAInfo']]],
+  ['reservedword_13',['reservedWord',['../structUDataInfo.html#af4a5b961c10058651aacb7a2ae45a199',1,'UDataInfo']]]
 ];
